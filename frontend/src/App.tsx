cd80d6b2cbbbeb30278e0d9cfb64850d5516f3fc
vagrant <vagrant@localhost.localdomain>
2021-03-27 08:50:40 +0000

    added frontend apps

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p>header</p>
+      </header>
+      <body>
+        <p>body</p>
+      </body>
+    </div>
+  );
+}
+
+export default App;
